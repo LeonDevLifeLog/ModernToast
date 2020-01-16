96de8c397316465b169d075e7d7db3f662f613f9
leon <leondevlifelog@gmail.com>
2020-01-16 09:02:35 +0800

    完善javadoc

--- lib-toast/src/main/java/com/github/leondevlifelog/toast/ModernToast.kt
+++ lib-toast/src/main/java/com/github/leondevlifelog/toast/ModernToast.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2020 Leon<leondevlifelog@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.github.leondevlifelog.toast
 
 import android.view.LayoutInflater
@@ -16,8 +32,10 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 
+/**
+ * 现代化的Toast
+ */
 class ModernToast(private val activity: FragmentActivity) {
-    val TAG: String = "ModernToast"
     var duration: Long = 1000
     var cancelable: Boolean = false
     var outSideTouchable: Boolean = true
@@ -28,8 +46,22 @@ class ModernToast(private val activity: FragmentActivity) {
     private var tvTip: TextView
     private var timer: Job? = null
 
+    /**
+     * 显示模式
+     */
     private enum class MODE {
-        LOADING, PROGRESS, TOAST
+        /**
+         * 无进度加载
+         */
+        LOADING,
+        /**
+         * 含进度加载
+         */
+        PROGRESS,
+        /**
+         * 普通Toast
+         */
+        TOAST
     }
 
     private val onBackPressed = object : OnBackPressedCallback(cancelable) {
@@ -57,6 +89,9 @@ class ModernToast(private val activity: FragmentActivity) {
         tvTip = contentView.findViewById(R.id.tvTip)
     }
 
+    /**
+     * 显示指定模式的Toast
+     */
     private fun show(mode: MODE, f: (ModernToast.() -> Unit)? = null): ModernToast {
         when (mode) {
             MODE.LOADING -> {
@@ -91,28 +126,57 @@ class ModernToast(private val activity: FragmentActivity) {
         return this
     }
 
+    /**
+     *显示成功提示的Toast
+     * @param id 提示字符资源id
+     * @param f 对显示配置的回调
+     * @return 返回Toast实例
+     */
     fun showSuccess(@StringRes id: Int, f: (ModernToast.() -> Unit)? = null): ModernToast {
         text = activity.getString(id)
         ivStatus.setImageResource(R.drawable.icon_success)
         return show(MODE.TOAST, f)
     }
 
+    /**
+     *显示成功提示的Toast
+     * @param text 提示字符
+     * @param f 对显示配置的回调
+     * @return 返回Toast实例
+     */
     fun showSuccess(text: String, f: (ModernToast.() -> Unit)? = null): ModernToast {
         ivStatus.setImageResource(R.drawable.icon_success)
         this.text = text
         return show(MODE.TOAST, f)
     }
 
+    /**
+     * 显示提示Toast
+     * @param f 对显示配置的回调
+     * @return 返回Toast实例
+     */
     fun showInfo(f: (ModernToast.() -> Unit)? = null): ModernToast {
         ivStatus.setImageResource(R.drawable.icon_info)
         return show(MODE.TOAST, f)
     }
 
+    /**
+     * 显示错误Toast
+     * @param f 对显示配置的回调
+     * @return 返回Toast实例
+     */
     fun showError(f: (ModernToast.() -> Unit)? = null): ModernToast {
         ivStatus.setImageResource(R.drawable.icon_error)
         return show(MODE.TOAST, f)
     }
 
+    /**
+     * 显示加载进度Toast
+     * @param text 提示字符
+     * @param progress 进度[0,100]
+     * @param f 对显示配置的回调
+     * @return 返回Toast实例
+     */
     fun showProgress(
         @IntRange(
             from = 0,
@@ -125,6 +189,13 @@ class ModernToast(private val activity: FragmentActivity) {
         return show(MODE.PROGRESS, f)
     }
 
+    /**
+     * 显示加载Toast
+     * @param text 提示字符
+     * @param duration 显示时长
+     * @param f 对显示配置的回调
+     * @return 返回Toast实例
+     */
     fun showLoading(
         text: String = "加载中...", duration: Long = 3000, f: (ModernToast.() -> Unit)? = null
     ): ModernToast {
@@ -134,6 +205,11 @@ class ModernToast(private val activity: FragmentActivity) {
         return show(MODE.LOADING, f)
     }
 
+    /**
+     *设置进度
+     * @param progress 具体进度[0,100]
+     * @return 返回Toast实例
+     */
     fun setProgress(
         @IntRange(
             from = 0,
